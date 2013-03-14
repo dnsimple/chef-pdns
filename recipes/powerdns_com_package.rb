c850d595884eaf7884f7ead2999aba501ac16997
Darrin Eden <darrin.eden@gmail.com>
2013-03-13 21:15:08 -0700

    install powerdns from powerdns.com

---
+++ recipes/powerdns_com_package.rb
@@ -0,0 +1,10 @@
+version = node[:pdns][:powerdns_com_package][:version]
+
+remote_file "#{Chef::Config[:file_cache_path]}/pdns-static_#{version}.deb" do
+  source "http://downloads.powerdns.com/releases/deb/pdns-static_#{version}.deb"
+  action :create_if_missing
+end
+
+dpkg_package 'pdns' do
+  source "#{Chef::Config[:file_cache_path]}/pdns-static_#{version}.deb"
+end
