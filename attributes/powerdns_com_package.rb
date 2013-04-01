1297a122cace0ed5fd10a5d93e43cd3317d9dd97
Darrin Eden <darrin.eden@gmail.com>
2013-04-01 16:24:15 -0700

    select package based on machine type

--- attributes/powerdns_com_package.rb
+++ attributes/powerdns_com_package.rb
@@ -1,1 +1,7 @@
-default[:pdns][:powerdns_com_package][:version] = '3.2-1_amd64'
+if node[:kernel][:machine] == 'x86_64'
+  machine = 'amd64'
+else
+  machine = 'i386'
+end
+
+default[:pdns][:powerdns_com_package][:version] = "3.2-1_#{machine}"
