09266ed453e43746e54dc85172b3a78a49b43789
jtimberman <joshua@opscode.com>
2013-05-27 21:38:47 -0600

    increment version for development

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures pdns"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.2"
+version          "0.1.3"
 depends "sqlite"
