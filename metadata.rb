b55833230f5fb0bc19a5514ada9fe3ac1f837144
Sean OMeara <someara@opscode.com>
2014-02-21 12:27:02 -0800

    Version bump to 0.3.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures pdns"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.1"
+version          "0.3.0"
 depends "sqlite"
 
 depends "build-essential"
