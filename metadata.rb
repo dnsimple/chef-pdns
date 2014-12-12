9a0f757b730125e83034eac095736fd30d1121d3
Jacobo García López de Araujo <jacobo.garcia@gmail.com>
2014-12-12 20:19:46 +0100

    Adding dependencies

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,13 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures pdns"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.4"
-depends "sqlite"
+version          "0.4.0"
 
+supports 'ubuntu'
+
+depends "sqlite"
 depends "build-essential"
 depends "mysql"
 depends "git"
 depends "resolvconf"
+
