c9459d116ac0658fbd196cb4f6e0c2ab3dcca932
Aaron Kalin <akalin@martinisoftware.com>
2014-12-14 17:25:13 +0100

    Extract more helpers for URL and filename
    These are used to calculate the filename and pathing for a source
    installation. Seems really insane, but it is better than attempting to
    calculate the version number and file name in the attributes file.

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -17,8 +17,20 @@
 # limitations under the License.
 #
 
-def pdns_file(uri)
+def pdns_pathname(uri)
   require 'pathname'
   require 'uri'
-  Pathname.new(URI.parse(uri).path).basename.to_s
+  Pathname.new(URI.parse(uri).path)
+end
+
+def pdns_file(uri)
+  pdns_pathname(uri).basename.to_s
+end
+
+def pdns_dir(uri)
+  path = pdns_pathname(uri)
+  extension = '.tar.bz2'
+  filename = path.basename.to_s
+  base_path = node['pdns']['authoritative']['source']['path']
+  "#{base_path}/#{::File.basename(filename, extension)}"
 end
