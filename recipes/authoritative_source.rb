86845b89c00f659dfbf293a7d2c6b0dafe23a5c6
Aaron Kalin <akalin@martinisoftware.com>
2014-12-14 19:11:12 +0100

    Remove and comment out older build method
    Switching from git based installation to a package with compile

--- recipes/authoritative_source.rb
+++ recipes/authoritative_source.rb
@@ -25,55 +25,76 @@ package 'libboost-all-dev'
 package 'ragel'
 package 'libpq-dev'
 
-execute 'pdns: bootstrap' do
-  command './bootstrap && ./bootstrap'
-  cwd path
-  creates '/usr/src/pdns/configure'
+# Base install directory
+pdns_basepath = node['pdns']['authoritative']['source']['path']
+# Filename
+pdns_filename = pdns_file(node['pdns']['authoritative']['source']['url'])
+# Base install dir + Filename
+pdns_filepath = "#{pdns_basepath}/#{pdns_filename}"
+# Base install dir + (Filename - Extension)
+pdns_dir = pdns_dir(pdns_filename)
+
+remote_file pdns_filepath do
+  source node['pdns']['authoritative']['source']['url']
+  action :create_if_missing
 end
 
-execute 'pdns: configure' do
-  command './configure ' +
-    "--with-modules='#{node['pdns']['source']['backends'].join(' ')}' " +
-    "--with-config-dir=#{node['pdns']['source']['config_dir']} " +
-    '--with-mysql-includes=/usr/include ' +
-    '--without-lua'
-  cwd path
-  creates '/usr/src/pdns/config.h'
+bash 'unarchive_source' do
+  cwd '/opt'
+  code <<-EOH
+  tar xjf #{::File.basename(pdns_filepath)} -C #{::File.dirname(pdns_filepath)}
+  EOH
+  not_if { ::File.directory?("#{pdns_dir}") }
 end
 
-execute 'pdns: build' do
-  command 'make'
-  cwd node['pdns']['source']['path']
-  creates '/usr/src/pdns/pdns/pdns_server'
-end
-
-execute 'pdns: install' do
-  command 'make install'
-  cwd node['pdns']['source']['path']
-  creates '/usr/local/sbin/pdns_server'
-end
-
-file '/usr/src/pdns/pdns/pdns' do
-  owner 'root'
-  group 'root'
+directory node['pdns']['authoritative']['config_dir'] do
+  owner node['pdns']['user']
+  group node['pdns']['group']
   mode '0755'
 end
 
-execute 'copy pdns init' do
-  command 'cp /usr/src/pdns/pdns/pdns /etc/init.d/pdns'
-  not_if 'diff /usr/src/pdns/pdns/pdns /etc/init.d/pdns'
-end
+# execute 'pdns: bootstrap' do
+#   command './bootstrap && ./bootstrap'
+#   cwd path
+#   creates '/usr/src/pdns/configure'
+# end
 
-file '/etc/init.d/pdns' do
-  owner 'root'
-  group 'root'
-  mode '0755'
-end
+# execute 'pdns: configure' do
+#   command './configure ' +
+#     "--with-modules='#{node['pdns']['source']['backends'].join(' ')}' " +
+#     "--with-config-dir=#{node['pdns']['source']['config_dir']} " +
+#     '--with-mysql-includes=/usr/include ' +
+#     '--without-lua'
+#   cwd path
+#   creates '/usr/src/pdns/config.h'
+# end
 
-directory '/etc/powerdns' do
-  owner 'pdns'
-  group 'pdns'
-  mode '0755'
-end
+# execute 'pdns: build' do
+#   command 'make'
+#   cwd node['pdns']['source']['path']
+#   creates '/usr/src/pdns/pdns/pdns_server'
+# end
 
+# execute 'pdns: install' do
+#   command 'make install'
+#   cwd node['pdns']['source']['path']
+#   creates '/usr/local/sbin/pdns_server'
+# end
+
+# file '/usr/src/pdns/pdns/pdns' do
+#   owner 'root'
+#   group 'root'
+#   mode '0755'
+# end
+
+# execute 'copy pdns init' do
+#   command 'cp /usr/src/pdns/pdns/pdns /etc/init.d/pdns'
+#   not_if 'diff /usr/src/pdns/pdns/pdns /etc/init.d/pdns'
+# end
+
+# file '/etc/init.d/pdns' do
+#   owner 'root'
+#   group 'root'
+#   mode '0755'
+# end
 
