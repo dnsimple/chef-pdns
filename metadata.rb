e62f99e2851f50c7ee1e6da19591d6f16ba4dc53
Aaron Kalin <akalin@martinisoftware.com>
2014-12-15 12:23:54 +0100

    Cleanup quotes

--- metadata.rb
+++ metadata.rb
@@ -1,15 +1,15 @@
-name             "pdns"
-maintainer       "Opscode, Inc."
-maintainer_email "cookbooks@opscode.com"
-license          "Apache 2.0"
-description      "Installs/Configures pdns"
+name             'pdns'
+maintainer       'Opscode, Inc.'
+maintainer_email 'cookbooks@opscode.com'
+license          'Apache 2.0'
+description      'Installs/Configures pdns'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.4.0"
+version          '0.4.0'
 
 supports 'ubuntu'
 
-depends "sqlite"
-depends "build-essential"
-depends "git"
-depends "mysql"
-depends "resolvconf"
+depends 'sqlite'
+depends 'build-essential'
+depends 'git'
+depends 'mysql'
+depends 'resolvconf'
