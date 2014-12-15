dffde2f55f06210edbad1195fba6d1c6fe261ef3
Aaron Kalin <akalin@martinisoftware.com>
2014-12-15 11:46:18 +0100

    Configure and setup for the pipe backend
    This is the simplest setup to configure PowerDNS authoritative servers
    by default.

--- attributes/authoritative.rb
+++ attributes/authoritative.rb
@@ -21,7 +21,7 @@ default['pdns']['authoritative']['config_dir'] = '/etc/powerdns'
 
 default['pdns']['authoritative']['source']['url'] = 'https://downloads.powerdns.com/releases/pdns-3.4.1.tar.bz2'
 default['pdns']['authoritative']['source']['path'] = '/opt'
-default['pdns']['authoritative']['source']['backends'] = %w( pipe gpgsql gmysql )
+default['pdns']['authoritative']['source']['backends'] = %w( pipe )
 
 default['pdns']['authoritative']['config']['config_dir'] = node['pdns']['authoritative']['config_dir']
 default['pdns']['authoritative']['config']['allow_recursion'] = [ '127.0.0.1' ]
@@ -32,5 +32,6 @@ default['pdns']['authoritative']['config']['setgid'] = node['pdns']['group']
 default['pdns']['authoritative']['config']['setuid'] = node['pdns']['user']
 default['pdns']['authoritative']['config']['version_string'] = 'powerdns'
 default['pdns']['authoritative']['config']['default_ttl'] = '3600'
+default['pdns']['authoritative']['config']['pipe_command'] = '/opt/pdns-3.4.1/modules/pipebackend/backend.pl'
 default['pdns']['authoritative']['config']['launch'] = 'pipe'
 
