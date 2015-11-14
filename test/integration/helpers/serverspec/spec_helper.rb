d6c54a905ccd683313ff399aef5e138b7d5d9c5d
Andrew Jones <andrew@jones.ec>
2015-11-14 16:19:07 -0500

    Fix recursor tests

--- test/integration/helpers/serverspec/spec_helper.rb
+++ test/integration/helpers/serverspec/spec_helper.rb
@@ -4,11 +4,11 @@ set :backend, :exec
 
 shared_examples_for 'a PowerDNS server' do
   it 'is running' do
-    expect(process('pdns_server')).to be_running
+    expect(process('pdns_server')).to(be_running)
   end
 
   it 'responds to a version query on 53' do
     expect(command('dig @localhost version.bind txt chaos').stdout).to \
-      match(/version.bind/).and match(/Served by PowerDNS/)
+      match(/version.bind/).and match(/PowerDNS/)
   end
 end
--- test/integration/recursor/serverspec/recursor_spec.rb
+++ test/integration/recursor/serverspec/recursor_spec.rb
@@ -1,10 +1,19 @@
 require 'spec_helper'
 
-describe 'A PowerDNS Resolver' do
-  it_behaves_like 'a PowerDNS server'
+describe 'a PowerDNS recursor' do
   
+  it 'is running' do
+    expect(process('pdns_recursor')).to(be_running)
+  end
+
+  it 'responds to a version query on 53' do
+    expect(command('dig @localhost version.bind txt chaos').stdout).to \
+      match(/version.bind/).and match(/PowerDNS/)
+  end
+
   it 'can resolve example.com' do
     expect(command('dig @localhost example.com').stdout).to \
       match(/Got answer:/)
   end
+
 end
