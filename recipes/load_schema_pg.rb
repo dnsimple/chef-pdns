1beda47e830111fb9e4ac6e1cf5f0c3c55c1795e
Jacobo García López de Araujo <jacobo.garcia@gmail.com>
2015-12-17 17:42:22 +0100

    Loading db schema and permissions for postgres/source recipes

---
+++ recipes/load_schema_pg.rb
@@ -0,0 +1,52 @@
+#
+# Cookbook Name:: pdns
+# Recipe:: _package
+#
+# Copyright 2015, Aetrion, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe 'database::postgresql'
+
+postgresql_connection_info = {
+  :host     => '127.0.0.1',
+  :port     => node['postgresql']['config']['port'],
+  :username => 'postgres',
+  :password => node['postgresql']['password']['postgres']
+}
+
+postgresql_database 'powerdns' do
+  connection postgresql_connection_info
+  action     :create
+end
+
+cookbook_file '/var/tmp/pdns_pg_schema.sql' do
+  source 'pg_schema.sql'
+  notifies :query, 'postgresql_database[powerdns]'
+  notifies :create, 'postgresql_database_user[pdns]'
+end
+
+postgresql_database 'powerdns' do
+  connection postgresql_connection_info
+  sql { ::File.open('/var/tmp/pdns_pg_schema.sql').read }
+  action :nothing
+end
+
+postgresql_database_user 'pdns' do
+  connection postgresql_connection_info
+  database_name 'powerdns'
+  username 'pdns'
+  password 'test'
+  action :nothing
+end
