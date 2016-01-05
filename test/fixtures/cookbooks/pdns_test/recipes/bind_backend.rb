649d5f2232bc3e14deadaff0a9e4f7e9a5f603a2
Jacobo García López de Araujo <jacobo.garcia@gmail.com>
2016-01-05 13:41:42 +0100

    Config file is now a template

--- test/fixtures/cookbooks/pdns_test/recipes/bind_backend.rb
+++ test/fixtures/cookbooks/pdns_test/recipes/bind_backend.rb
@@ -1,6 +1,6 @@
-cookbook_file '/etc/pdns/bind-backend.conf' do
-  source 'bind-backend.conf'
-  owner 'root'
-  group 'root'
+template '/etc/powerdns/bind-backend.conf' do
+  source 'bind-backend.conf.erb'
+  owner 'pdns'
+  group 'pdns'
   mode 00755
 end
