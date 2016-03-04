a0157b6d7a13a9bff96747fe3cfad2aabb027f6e
Aaron Kalin <akalin@martinisoftware.com>
2016-03-03 21:42:22 -0600

    Version 2.1.1 lets GO!

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
 description      'Installs/Configures pdns'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.1.0'
+version          '2.1.1'
 
 supports 'ubuntu', '= 12.04'
 supports 'ubuntu', '= 14.04'
