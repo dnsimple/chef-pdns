5f573d287c5ce4c4f61cd25cd785514fb79ec96b
Aaron Kalin <akalin@martinisoftware.com>
2016-03-03 23:24:43 -0600

    Release 2.2.1, now with slightly less bugs

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
 description      'Installs/Configures pdns'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.2.0'
+version          '2.2.1'
 source_url       'https://github.com/aetrion/pdns' if respond_to?(:source_url)
 issues_url       'https://github.com/aetrion/pdns/issues' if respond_to?(:issues_url)
 
