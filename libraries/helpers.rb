db1eb25a9fa1c9debdd95e148adbd9cd91ec86d4
Aaron Kalin <akalin@martinisoftware.com>
2016-10-06 11:07:25 -0500

    Comment out most of library code
    This is mostly useful for source installs and the package stuff will get
    baked in by platform :+1:

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -17,56 +17,56 @@
 # limitations under the License.
 #
 
-def pdns_pathname(uri)
-  require 'pathname'
-  require 'uri'
-  Pathname.new(URI.parse(uri).path)
-end
+# def pdns_pathname(uri)
+#   require 'pathname'
+#   require 'uri'
+#   Pathname.new(URI.parse(uri).path)
+# end
 
-def pdns_file(uri)
-  pdns_pathname(uri).basename.to_s
-end
+# def pdns_file(uri)
+#   pdns_pathname(uri).basename.to_s
+# end
 
-def pdns_dir(uri)
-  path = pdns_pathname(uri)
-  extension = '.tar.bz2'
-  filename = path.basename.to_s
-  base_path = node['pdns']['source']['path']
-  "#{base_path}/#{::File.basename(filename, extension)}"
-end
+# def pdns_dir(uri)
+#   path = pdns_pathname(uri)
+#   extension = '.tar.bz2'
+#   filename = path.basename.to_s
+#   base_path = node['pdns']['source']['path']
+#   "#{base_path}/#{::File.basename(filename, extension)}"
+# end
 
-def pdns_package_module_requirements
-  modules = node['pdns']['authoritative']['backends']
-  required_packages = []
-  modules.each do |mod|
-    case mod
-    when 'gpgsql'
-      required_packages << 'pdns-backend-pgsql'
-    when 'gmysql'
-      required_packages << 'pdns-backend-mysql'
-    when 'random'
-      # Random isn't available on Ubuntu
-      next
-    when 'bind'
-      required_packages # Nothing extra is needed here
-    else
-      required_packages << "pdns-backend-#{mod}"
-    end
-  end
-  required_packages
-end
+# def pdns_package_module_requirements
+#   modules = node['pdns']['authoritative']['backends']
+#   required_packages = []
+#   modules.each do |mod|
+#     case mod
+#     when 'gpgsql'
+#       required_packages << 'pdns-backend-pgsql'
+#     when 'gmysql'
+#       required_packages << 'pdns-backend-mysql'
+#     when 'random'
+#       # Random isn't available on Ubuntu
+#       next
+#     when 'bind'
+#       required_packages # Nothing extra is needed here
+#     else
+#       required_packages << "pdns-backend-#{mod}"
+#     end
+#   end
+#   required_packages
+# end
 
-def pdns_source_module_requirements
-  modules = node['pdns']['authoritative']['backends']
-  required_packages = []
-  modules.each do |mod|
-    case mod
-    when 'gpgsql'
-      required_packages << 'libpq-dev'
-    when 'gmysql'
-      required_packages << 'libmysqlclient-dev'
-    end
-  end
-  required_packages
-end
+# def pdns_source_module_requirements
+#   modules = node['pdns']['authoritative']['backends']
+#   required_packages = []
+#   modules.each do |mod|
+#     case mod
+#     when 'gpgsql'
+#       required_packages << 'libpq-dev'
+#     when 'gmysql'
+#       required_packages << 'libmysqlclient-dev'
+#     end
+#   end
+#   required_packages
+# end
 
