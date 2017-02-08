8ea81bd81778ffbc438f54e80782fd6be153669e
Jacobo García López de Araujo <jacobo.garcia@gmail.com>
2017-02-08 17:37:36 +0100

    New release 2.5.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
 description      'Installs/Configures pdns'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.4.1'
+version          '2.5.0'
 source_url       'https://github.com/dnsimple/pdns' if respond_to?(:source_url)
 issues_url       'https://github.com/dnsimple/pdns/issues' if respond_to?(:issues_url)
 
