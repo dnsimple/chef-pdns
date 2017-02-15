f483222c188d212915a3cc969b096b8be4273d8e
Jacobo García López de Araujo <jacobo.garcia@gmail.com>
2017-02-15 19:18:11 +0100

    Adding a second integration test

--- test/integration/recursor/default_spec.rb
+++ test/integration/recursor/default_spec.rb
@@ -1,3 +1,8 @@
 describe package('pdns-recursor') do
   it { should be_installed }
 end
+
+describe port(53) do
+  it { should be_listening }
+  its('processes') { should include 'pdns_recursor' }
+end
