ea1b57e7428c6d63f7b61b016c980fb31981e70b
Jacobo García López de Araujo <jacobo.garcia@gmail.com>
2017-02-21 10:36:33 +0100

    For now default spec is empty

--- test/integration/recursor/default_spec.rb
+++
@@ -1,8 +0,0 @@
-describe package('pdns-recursor') do
-  it { should be_installed }
-end
-
-describe port(53) do
-  it { should be_listening }
-  its('processes') { should include 'pdns_recursor' }
-end
