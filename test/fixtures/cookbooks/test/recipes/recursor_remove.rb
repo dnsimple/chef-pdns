974e3dfe53feea568aa911ea1c92942774a108d4
Jacobo García López de Araujo <jacobo.garcia@gmail.com>
2017-02-21 12:22:19 +0100

    Adding uninstall action instead of remove

--- test/fixtures/cookbooks/test/recipes/recursor_remove.rb
+++ test/fixtures/cookbooks/test/recipes/recursor_remove.rb
@@ -1,15 +1,4 @@
-def version_per_platform
-  case node['platform']
-  when 'debian'
-    '4.0.3-1pdns.jessie'
-  when 'ubuntu'
-    "4.0.4-1pdns.#{node['lsb']['codename']}"
-  when 'rhel'
-    "4.0.4-1pdns.el#{node['centos-release']['version']}"
-  end
-end
-
 pdns_recursor 'a_pdns_recursor' do
-  action :remove
+  action :uninstall
   version version_per_platform
 end
