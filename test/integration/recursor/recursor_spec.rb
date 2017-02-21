ebebab88f9e757b1b8ca4ff3c04669347f9e1243
Jacobo García López de Araujo <jacobo.garcia@gmail.com>
2017-02-21 11:48:52 +0100

    This set of tests work for the recursor

---
+++ test/integration/recursor/recursor_spec.rb
@@ -0,0 +1,31 @@
+def default_recursor_user
+  if os[:family] == 'debian'
+    return 'pdns'
+  elsif os['platform_name'] == 'redhat'
+    return  'powerdns-recursor'
+  end
+end
+
+describe package('pdns-recursor') do
+  it { should be_installed }
+end
+
+describe port(53) do
+  it { should be_listening }
+  its('processes') { should include 'pdns_recursor' }
+end
+
+describe user(default_recursor_user) do
+  it { should exist }
+end
+
+describe group(default_recursor_user) do
+  it { should exist }
+end
+
+describe command('dig @127.0.0.1 dnsimple.com') do
+  its('stdout') { should match(//) }
+end
+
+## TO-DO
+## Add a domain to the configuration (probably bind backend or pipe backend and dig for it)
