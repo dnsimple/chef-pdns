6a447f3ae3090cdc0408a2e8e40c96b47943b073
Jacobo García López de Araujo <jacobo.garcia@gmail.com>
2017-02-22 21:56:57 +0100

    Adding matchers

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -3,6 +3,14 @@ if defined?(ChefSpec)
     ChefSpec::Matchers::ResourceMatcher.new(:pdns_recursor, :install, resource_name)
   end
 
+  def pdns_recursor_config(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:pdns_recursor, :config, resource_name)
+  end
+
+  def pdns_recursor_service(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:pdns_recursor, :service, resource_name)
+  end
+
   def pdns_recursor_remove(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(:pdns_recursor, :remove, resource_name)
   end
