d6629e53386988151ce44bc5d219fe28a2be93cb
Jacobo García López de Araujo <jacobo.garcia@gmail.com>
2017-03-01 11:20:43 +0100

    Calling to the right rhel fixture

--- spec/unit/recipes/recursor_rhel_spec.rb
+++ spec/unit/recipes/recursor_rhel_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'test::recursor_rhel' do
+describe 'test::recursor_install_rhel' do
   context 'on rhel platform' do
     let(:rhel_runner) do
       ChefSpec::SoloRunner.new(
