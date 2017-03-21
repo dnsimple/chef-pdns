8495a070c13dbadfdf1c00ee9acf7b6dc3c134a6
Jacobo García López de Araujo <jacobo.garcia@gmail.com>
2017-03-21 12:13:53 +0100

    Moved the test cookbook to cookbooks

--- test/cookbooks/pdns_test/metadata.rb
+++ test/cookbooks/pdns_test/metadata.rb
@@ -2,7 +2,7 @@ name             'pdns_test'
 maintainer       'Aetrion, LLC DBA DNSimple'
 maintainer_email 'support@dnsimple.com'
 license          'Apache 2.0'
-description      'A test fixture cookbook for the pdns cookbook'
+description      'A test cookbook for the pdns cookbook'
 version          '0.0.1'
 
 depends 'pdns'
