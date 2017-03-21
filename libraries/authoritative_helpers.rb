be7f1536c37e7b43ce53f4718b42936d8fdd68e7
Jacobo García López de Araujo <jacobo.garcia@gmail.com>
2017-03-21 15:02:12 +0100

    Initial authoritative resource

---
+++ libraries/authoritative_helpers.rb
@@ -0,0 +1,36 @@
+#
+# Cookbook Name:: pdns
+# Libraries:: authoritive_helpers
+#
+# Copyright 2014, Aetrion, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+def default_authoritative_config_directory
+  case node['platform_family']
+  when 'debian'
+    '/etc/powerdns'
+  when 'rhel'
+    '/etc/pdns'
+  end
+end
+
+def default_authoritative_run_user
+  case node['platform_family']
+  when 'debian'
+    'pdns'
+  when 'rhel'
+    'pdns'
+  end
+end
