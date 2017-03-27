0fb645063f378f0a7db133e74ec94767a7df0f81
Jacobo García López de Araujo <jacobo.garcia@gmail.com>
2017-03-27 22:48:38 +0200

    --config-name option only works with hyphens

--- test/cookbooks/pdns_test/recipes/authoritative_install_single.rb
+++ test/cookbooks/pdns_test/recipes/authoritative_install_single.rb
@@ -1,16 +1,12 @@
-pdns_authoritative_install 'a_pdns_authoritative' do
+pdns_authoritative_install 'a-pdns-authoritative' do
   action :install
   version authoritative_version_per_platform
 end
 
-pdns_authoritative_service 'a_pdns_authoritative' do
+pdns_authoritative_service 'a-pdns-authoritative' do
   action [:enable, :start]
 end
 
-pdns_authoritative_config 'a_pdns_authoritative' do
+pdns_authoritative_config 'a-pdns-authoritative' do
   action :create
-end
-
-pdns_authoritative_service 'a_pdns_authoritative' do
-  action :restart
 end
