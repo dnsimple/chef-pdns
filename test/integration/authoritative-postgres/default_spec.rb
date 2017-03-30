8ab76960f360ef623dd20f7ed90d6db76b800a84
Jacobo García López de Araujo <jacobo.garcia@gmail.com>
2017-03-30 12:20:21 +0200

    Adding integration specs for bind and postgres

---
+++ test/integration/authoritative-postgres/default_spec.rb
@@ -0,0 +1,34 @@
+require File.expand_path('test/libraries/helpers.rb')
+
+describe package('pdns-server') do
+  it { should be_installed }
+end
+
+describe package('pdns-backend-pgsql') do
+  it { should be_installed }
+end
+
+describe port(53) do
+  it { should be_listening }
+  its('processes') { should match(['pdns_server-a']) }
+end
+
+describe user(default_run_user) do
+  it { should exist }
+end
+
+describe group(default_run_user) do
+  it { should exist }
+end
+
+describe processes('pdns_server-authoritative-server-01-instance') do
+  its ('users') { should eq ["#{default_run_user}"] }
+end
+
+describe command('dig chaos txt version.bind @127.0.0.1 +short') do
+  its('stdout.chomp') { should match('"PowerDNS Authoritative Server 4.0.3 (built Jan 17 2017 09:06:22 by root@d087975b3e20)"') }
+end
+
+describe command('dig @127.0.0.1 smoke.example.org') do
+  its('stdout.chomp') { should match(/127.0.0.123/) }
+end
