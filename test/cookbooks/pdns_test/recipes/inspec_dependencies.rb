863e65ee8de589697038b75909c4eed4e0b124a8
Jacobo García López de Araujo <jacobo.garcia@gmail.com>
2017-05-24 18:19:24 +0200

    Removing docker only dependencies
    They have been moved to kitchen.dokken.yml 
    
    I kept installing dig here for rhel since it is
    a specific dependency of our tests

--- test/cookbooks/pdns_test/recipes/inspec_dependencies.rb
+++ test/cookbooks/pdns_test/recipes/inspec_dependencies.rb
@@ -1,18 +1,4 @@
-package 'lsof' do
-  action :install
-end
-
 package 'bind-utils' do
   action :install
   only_if { node['platform_family'] == 'rhel' }
-end
-
-package 'net-tools' do
-  action :install
-  only_if { node['platform_family'] == 'debian' }
-end
-
-package 'dnsutils' do
-  action :install
-  only_if { node['platform_family'] == 'debian' }
 end
