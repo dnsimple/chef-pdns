93fe88a4574cc374d3258d7f8d1849e1c3da14c0
Aaron Kalin <akalin@martinisoftware.com>
2017-07-28 12:24:07 -0500

    Test for a raised validation exception

--- spec/recipes/hyphens_spec.rb
+++ spec/recipes/hyphens_spec.rb
@@ -1,19 +1,20 @@
 require 'spec_helper'
 
 describe 'pdns_test::hyphens' do
-  let(:runner) do
+  let(:chef_run) do
     ChefSpec::SoloRunner.new(
       platform: 'ubuntu',
       version: '16.04',
       os: 'linux',
-      step_into: %w(pdns_authoritative_config pdns_authoritative_service pdns_recursor_config pdns_recursor_service))
+      step_into: ['pdns_authoritative_config']
+    ).converge(described_recipe)
   end
 
-  let(:chef_run) { runner.converge(described_recipe) }
-
-  context('when creating a configuration for an authoritative') do
+  context('when configured with a hyphen in the name') do
     it 'returns an exception' do
-      expect { chef_run }.to raise_error
+      expect {
+        chef_run.pdns_authoritative_config('with-hyphen').instance_name
+      }.to raise_error(Chef::Exceptions::ValidationFailed)
     end
   end
 end
