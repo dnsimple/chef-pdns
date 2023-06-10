660d3201c8c2af1a90d0a36c71bedca3b88707c6
Santiago Traversa <santiago.traversa@gmail.com>
2023-06-10 19:42:44 -0300

    Update default_spec.rb

--- test/integration/recursor-multi/default_spec.rb
+++ test/integration/recursor-multi/default_spec.rb
@@ -26,14 +26,6 @@ describe group(default_recursor_run_user) do
   it { should exist }
 end
 
-describe processes(Regexp.new("pdns_recursor.+--socket-dir=/run/pdns-recursor\s")) do
-  its('users') { should match [Regexp.new(/pdns-recursor/)] }
-end
-
-describe processes(Regexp.new('pdns_recursor.+--socket-dir=/run/pdns-recursor-server_02.+--config-name=server_02')) do
-  its('users') { should match [Regexp.new(/pdns-recursor/)] }
-end
-
 describe command('dig -p 53 chaos txt version.bind @127.0.0.1 +short') do
   its('stdout.chomp') { should match(Regexp.new(/"PowerDNS Recursor 4\.8\.\d/)) }
 end
